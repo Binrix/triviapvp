f5e553b83539bc6b9fb98b9b4d297d21d10881f3
Capi2k <mad.lutz2000rgmail.com>
2023-05-15 16:40:10 +0200

    add missing app.module import

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -16,7 +17,8 @@ import { RegistrationComponent } from './registration/registration.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
