b665341260d02ce4b08eb57fbba46cfb575ccdce
Capi2k <mad.lutz2000rgmail.com>
2023-05-24 19:37:58 +0200

    add back button to impressum view

--- frontend/src/app/impressum/impressum.component.ts
+++ frontend/src/app/impressum/impressum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-impressum',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImpressumComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
